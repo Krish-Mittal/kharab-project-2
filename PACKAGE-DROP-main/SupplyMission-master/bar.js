cb9941311ce2138718575bfb814e72853bba352d
Krish-Mittal <69497201+Krish-Mittal@users.noreply.github.com>
2020-10-17 16:16:06 +0530

    Add files via upload

---
+++ PACKAGE-DROP-main/SupplyMission-master/bar.js
@@ -0,0 +1,20 @@
+class bar
+{
+    constructor(x,y,w,h)
+    {
+        var op=
+        {
+            isStatic:true
+        }
+        this.body=Bodies.rectangle(x,y,w,h,op);
+        this.h=h;
+        this.w=w;
+        World.add(world,this.body);
+    }
+    display()
+    {
+     
+        rectMode(CENTER);
+        rect(this.body.position.x,this.body.position.y,this.w,this.h);
+    }
+}
